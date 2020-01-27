15f89e8d50bd54fcdd82a67c9bb57389a1c7008e
maricris <maricris.danalex@gmail.com>
2020-01-27 14:54:54 +0800

    recommit

--- storage/framework/views/6ef25eb92154b40dd568169e1abbf5275417f311.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <strong>Copyright &copy; <?php echo e(Carbon\Carbon::now()->format('Y')); ?> <a href="https://new.danalexintl.com"><span style="color: #000;">DAN</span><span style="color: #034802 ;">ALEX</span> Corporation</a></strong> All rights
-      reserved.
-    </div>
-    
-  </footer>
